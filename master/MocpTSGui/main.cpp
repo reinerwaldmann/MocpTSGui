fd1b8db18237184f7c22d1e6f27185d432faedec
reinerwaldmann <mksd@list.ru>
2013-10-23 17:09:55 +0400

    Successfully played with qss
    Qss may be applied from file black.qss, which is to be put where the
    executable is been executed. All comments considering qss in main.cpp.

--- master/MocpTSGui/main.cpp
+++ master/MocpTSGui/main.cpp
@@ -1,9 +1,39 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+#include "QFile"
+#include  <iostream>
+#include <QTextStream>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+
+    QFile file("black.qss");
+       if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+
+    {
+        qDebug ("Error in opening stylesheet file");
+    }
+
+       QString StyleSheet;
+
+       QTextStream in(&file); //File.readall DOESN'T WORK FSR
+       //QString StyleSheet = QLatin1String(file.readAll()); THIS DOESN'T WORK,
+
+       while (!in.atEnd()) {
+           QString line = in.readLine();
+           StyleSheet.append(line+'\n');
+       }
+
+       //sets stylesheet application-wide
+    a.setStyleSheet(StyleSheet);
+
+        //sets stylesheet window-wide
+    //this prevents applying style to children windows
+    //materials considering qss stuff are in google drive
+
+    //  w.setStyleSheet(StyleSheet);
+
 
     bool deb=0;
     if (argc)
@@ -16,6 +46,12 @@ int main(int argc, char *argv[])
         }
 
     MainWindow w(deb);
+
+
+//qDebug(StyleSheet.toStdString().c_str()); //debugging a QString
+
+
+
 
     w.show();
     
